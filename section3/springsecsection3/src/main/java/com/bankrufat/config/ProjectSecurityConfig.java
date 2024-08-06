4f4c9f97ee0aed6e2d9c68ed9b925b70d96ce06d
rufat_rustam <rufikrustamli96@gmail.com>
2024-08-06 20:48:45 +0400

    section3 created and added some users , compromisedpassCheck and passwordencoder using by PasswordEncoderFactories

--- section3/springsecsection3/src/main/java/com/bankrufat/config/ProjectSecurityConfig.java
+++ section3/springsecsection3/src/main/java/com/bankrufat/config/ProjectSecurityConfig.java
@@ -4,8 +4,17 @@ import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -17,9 +26,7 @@ public class ProjectSecurityConfig {
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
 //        http.authorizeHttpRequests((requests) -> requests.anyRequest().denyAll());
 //        http.authorizeHttpRequests((requests) -> requests.anyRequest().permitAll());
-        http.authorizeHttpRequests((requests) -> requests.requestMatchers("/myAccount",
-                "/myBalance", "/myCards" , "/myLoans").authenticated()
-                .requestMatchers("/contact","/notices","/error").permitAll());
+        http.authorizeHttpRequests((requests) -> requests.requestMatchers("/myAccount", "/myBalance", "/myCards", "/myLoans").authenticated().requestMatchers("/contact", "/notices", "/error").permitAll());
 
 
 //        http.formLogin(flc->flc.disable());
@@ -27,5 +34,28 @@ public class ProjectSecurityConfig {
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
+    }
+
+
+    @Bean
+    public UserDetailsService getUserDetails() {
+        UserDetails user = User.withUsername("user")
+                .password("{noop}RufatRustam@12345")
+                .authorities("read")
+                .build();
+        UserDetails admin = User.withUsername("admin")
+                .password("{bcrypt}$2a$12$eZU7x0HVbnpN..C39IHhn.b82rEzZKOdp5MfpfE0.4JkFm4jPDqMi")
+                .authorities("admin").build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    CompromisedPasswordChecker compromisedPasswordChecker(){
+        return new HaveIBeenPwnedRestApiPasswordChecker();
     }
 }
