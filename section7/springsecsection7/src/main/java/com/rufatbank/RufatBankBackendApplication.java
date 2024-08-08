3e2590f1b266ef3cd1a270d83aa08375fdc7638a
rufat_rustam <rufikrustamli96@gmail.com>
2024-08-08 21:13:32 +0400

    i download section 7 from github which one is eazy bytes . there are mvc service front codes for ui  eazyschool web site

---
+++ section7/springsecsection7/src/main/java/com/rufatbank/RufatBankBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rufatbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RufatBankBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RufatBankBackendApplication.class, args);
+    }
+
+}
---
+++ section7/springsecsection7/src/main/java/com/rufatbank/repository/CustomerRepository.java
@@ -0,0 +1,14 @@
+package com.rufatbank.repository;
+
+import com.rufatbank.model.Customer;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CustomerRepository extends CrudRepository<Customer,Long> {
+
+    Optional<Customer> findByEmail(String email);
+
+}
