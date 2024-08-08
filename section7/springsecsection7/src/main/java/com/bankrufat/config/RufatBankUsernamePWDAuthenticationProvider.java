998038320fe5df74829523bac80061baab4e734f
rufat_rustam <rufikrustamli96@gmail.com>
2024-08-08 12:42:42 +0400

    diqqet papka 5 ve papka 6 icerikleri 1 1leri ile evez olundu section7 sadece yaradildi

---
+++ section7/springsecsection7/src/main/java/com/bankrufat/config/RufatBankUsernamePWDAuthenticationProvider.java
@@ -0,0 +1,39 @@
+package com.bankrufat.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@Profile("!prod")
+@RequiredArgsConstructor
+public class RufatBankUsernamePWDAuthenticationProvider implements AuthenticationProvider {
+
+    private final UserDetailsService userDetailsService;
+    private final PasswordEncoder pwdEncoder;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String userName = authentication.getName();
+        String pwd = authentication.getCredentials().toString();
+        UserDetails userDetails = userDetailsService.loadUserByUsername(userName);
+        if (pwdEncoder.matches(pwd, userDetails.getPassword())) {
+            return new UsernamePasswordAuthenticationToken(userName, pwd, userDetails.getAuthorities());
+        } else {
+            throw new BadCredentialsException("password incorrect!");
+        }
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return (UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication));
+    }
+}
---
+++ section7/springsecsection7/src/main/resources/sql/scripts.sql
@@ -0,0 +1,25 @@
+create table users(username varchar(50) not null primary key,password varchar(500) not null,enabled boolean not null);
+create table authorities (username varchar(50) not null,authority varchar(50) not null,constraint fk_authorities_users foreign key(username) references users(username));
+create unique index ix_auth_username on authorities (username,authority);
+
+
+INSERT IGNORE INTO users VALUES ('user', '{noop}RufatRustam@12345', '1');
+INSERT IGNORE INTO authorities VALUES ('user', 'read');
+
+INSERT IGNORE INTO `users`
+VALUES
+     ('admin', '{bcrypt}$2a$12$eZU7x0HVbnpN..C39IHhn.b82rEzZKOdp5MfpfE0.4JkFm4jPDqMi', '1');
+INSERT IGNORE INTO `authorities` VALUES ('admin', 'admin');
+
+
+CREATE TABLE `customer` (
+                            `id` int NOT NULL AUTO_INCREMENT,
+                            `email` varchar(45) NOT NULL,
+                            `pwd` varchar(200) NOT NULL,
+                            `role` varchar(45) NOT NULL,
+                            PRIMARY KEY (`id`)
+);
+
+INSERT  INTO customer (email, pwd, role)  VALUES ('rufat@mail.ru', '{noop}RufatRustam@12345', 'read');
+INSERT  INTO customer (email, pwd, role) VALUES
+                          ('admin@mail.ru', '{bcrypt}$2a$12$eZU7x0HVbnpN..C39IHhn.b82rEzZKOdp5MfpfE0.4JkFm4jPDqMi', 'admin');
